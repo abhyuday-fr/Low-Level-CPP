2bbd029adba7cfc13c848e71bdb4d64aec8b7956
abhyuday-fr <abhyuday-fr@gmail.com>
2026-05-06 15:35:36 +0530

    full std::function custom implementation

--- my::function/main.cpp
+++ my::function/main.cpp
@@ -1,12 +1,18 @@
 #include "my_function.hpp"
 #include <iostream>
 
+void free_function(int x) {
+  std::cout << "Free fnction called with: " << x << "\n";
+}
+
 int main() {
+  my::function<void(int)> f1 = free_function;
+  f1(10);
 
-  int x{0};
-  my::function<void(int)> f = [&](int x) { x += 1; };
+  int multiplier = 5;
+  my::function<int(int)> f2 = [multiplier](int x) { return x * multiplier; };
 
-  std::cout << x << "\n";
+  std::cout << "Lambda returned: " << f2(10) << "\n";
 
   return 0;
 }
