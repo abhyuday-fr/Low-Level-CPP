51b0855fef704acff4a0452bec3b9052bfbd160f
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-09-05 19:54:27 +0530

    Added arena allocator

---
+++ ReinC/arena.c
@@ -0,0 +1,50 @@
+#include "arena.h"
+#include "base.h"
+#include <stdlib.h>
+
+mem_arena *arena_create(u64 capacity) {
+  mem_arena *arena = malloc(sizeof(mem_arena));
+  if (arena == NULL) {
+    return NULL;
+  }
+
+  arena->memory = malloc(capacity);
+  if (arena->memory == NULL) {
+    free(arena);
+    return NULL;
+  }
+
+  arena->capacity = capacity;
+  arena->pos = 0;
+
+  return arena;
+}
+
+void arena_destroy(mem_arena *arena) {
+  if (arena == NULL) {
+    return;
+  }
+  free(arena->memory);
+  free(arena);
+}
+
+void *arena_push(mem_arena *arena, u64 size, b32 zero) {
+  if (arena == NULL) {
+    return NULL;
+  }
+
+  u64 pos_aligned = ALIGN_UP_POW2(arena->pos, ARENA_ALIGN);
+  if (pos_aligned > arena->capacity || size > arena->capacity - pos_aligned) {
+    return NULL;
+  }
+
+  u64 new_pos = pos_aligned + size;
+  u8 *out = arena->memory + pos_aligned;
+  arena->pos = new_pos;
+
+  if (zero) {
+    memset(out, 0, size);
+  }
+
+  return out;
+}
---
+++ ReinC/arena.h
@@ -0,0 +1,23 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include "base.h"
+
+#define ARENA_ALIGN (sizeof(void *))
+
+typedef struct {
+  u8 *memory;
+  u64 capacity;
+  u64 pos;
+} mem_arena;
+
+mem_arena *arena_create(u64 capacity);
+void arena_destroy(mem_arena *arena);
+void *arena_push(mem_arena *arena, u64 size, b32 zero);
+
+#define PUSH_STRUCT(arena, T) (T *)arena_push((arena), sizeof(T), true)
+#define PUSH_ARRAY(arena, T, n) (T *)arena_push((arena), sizeof(T) * n, true)
+#define PUSH_ARRAY_UNINIT(arena, T, n)                                         \
+  (T *)arena_push((arena), sizeof(T) * n, false)
+
+#endif
